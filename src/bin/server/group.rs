cc7cf83b75bed29f164acc8605c3ff4214e17936
chojuninengu <chojuninengu@gmail.com>
2025-04-03 00:24:26 +0100

    feat(group): implement message handling for subscribers in Group

--- src/bin/server/group.rs
+++ src/bin/server/group.rs
@@ -1,11 +1,14 @@
 use crate::connection::Outbound;
+use async_chat::FromServer;
 use async_std::task;
 use std::sync::Arc;
 use tokio::sync::broadcast;
+
 pub struct Group {
     name: Arc<String>,
     sender: broadcast::Sender<Arc<String>>,
 }
+
 impl Group {
     pub fn new(name: Arc<String>) -> Group {
         let (sender, _receiver) = broadcast::channel(1000);
@@ -25,5 +28,16 @@ async fn handle_subscriber(
     mut receiver: broadcast::Receiver<Arc<String>>,
     outbound: Arc<Outbound>,
 ) {
-    todo!()
+    while let Ok(message) = receiver.recv().await {
+        let response = FromServer::Message {
+            group_name: group_name.clone(),
+            message: message.clone(),
+        };
+        
+        if let Err(e) = outbound.send(response).await {
+            eprintln!("Error sending message to client: {}", e);
+            // If we can't send to this client, we should probably remove them from the group
+            break;
+        }
+    }
 }
