84e3b85905859965b6fb797bd7d3917d9402c635
chojuninengu <chojuninengu@gmail.com>
2025-04-03 00:31:28 +0100

    Feat(user): integrate user management into server connection handling

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,6 +1,7 @@
 pub mod connection;
 pub mod group_table;
 pub mod group;
+pub mod user;
 
 use connection::serve;
 
@@ -16,14 +17,17 @@ fn main() -> anyhow::Result<()> {
     ADDRESS",
     );
     let chat_group_table = Arc::new(group_table::GroupTable::new());
+    let user_manager = Arc::new(user::UserManager::new());
+
     async_std::task::block_on(async {
         let listener = TcpListener::bind(address).await?;
         let mut new_connections = listener.incoming();
         while let Some(socket_result) = new_connections.next().await {
             let socket = socket_result?;
             let groups = chat_group_table.clone();
+            let users = user_manager.clone();
             task::spawn(async {
-                log_error(serve(socket, groups).await);
+                log_error(serve(socket, groups, users).await);
             });
         }
         Ok(())
